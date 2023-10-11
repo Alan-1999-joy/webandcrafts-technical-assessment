f48997b122dbb53cbb2bce9fad9aa9241820a774
Alan Joy <114681209+Alan-1999-joy@users.noreply.github.com>
2023-10-11 14:29:38 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    center: true,
+    loop:false,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:1
+      },
+      1000:{
+        items:2
+      }
+  }
+  })
+});
